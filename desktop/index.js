b375ec4b68be94e0295aed7dc6b9c4dad6b30bfd
Alex <alex.barry@gmail.com>
2016-07-25 09:38:10 -0400

    Purging everything of coffeescript.

--- desktop/index.js
+++ desktop/index.js
@@ -1,3 +1,24 @@
-require('coffee-script').register();
-require('./application.coffee');
+const electron = require('electron')
+const { crashReporter, app, BrowserWindow } = electron
+const path = require('path')
 
+crashReporter.start()
+
+var mainWindow = null
+
+app.on('window-all-closed', function () {
+  app.quit() unless process.platform == 'darwin'
+})
+
+app.on('ready', function () {
+  var mainWindow = new BrowserWindow({
+    width: 398
+    height: 600
+  })
+
+  mainWindow.loadURL('file://' + path.resolve(__dirname, '..', 'public', 'index.html'))
+
+  mainWindow.on('close', function () {
+    mainWindow = null
+  })
+})
